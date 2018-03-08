0d3f77ae6c66d1bcecb28259ce9438186e433685
Alvintang6 <alvin.tang6@gmail.com>
2018-03-08 14:55:24 -0500

    add class

--- src/pcl_write.cpp
+++ src/pcl_write.cpp
@@ -5,19 +5,47 @@
 #include <sensor_msgs/PointCloud2.h>
 #include <pcl/io/pcd_io.h>
 
+using namespace std;
+class cloudHandler
+{
+public:
+	cloudHandler()
+	: frame(0)
+	{
+		pcl_sub = nh.subscribe("kinect2/sd/points", 10, &cloudHandler::cloudCB, this);
+		
+	}
+
+
+
 void cloudCB(const sensor_msgs::PointCloud2 &input)
 {
+    
+    oss.str("");
+    oss << "./" << std::setfill('0') << std::setw(4) << frame;
+    const std::string baseName = oss.str();
+    const std::string cloudName = baseName + "_cloud.pcd";
+
     pcl::PointCloud<pcl::PointXYZ> cloud;
     pcl::fromROSMsg(input, cloud);
-    pcl::io::savePCDFileASCII ("write_pcd_test.pcd", cloud);
+    pcl::io::savePCDFileASCII (cloudName, cloud);
+    ++frame;
 }
+
+protected:
+	ros::NodeHandle nh;
+	std::ostringstream oss;
+	ros::Subscriber pcl_sub;
+	size_t frame;
+
+};
 
 main (int argc, char **argv)
 {
     ros::init (argc, argv, "pcl_write");
 
-    ros::NodeHandle nh;
-    ros::Subscriber bat_sub = nh.subscribe("kinect2/sd/points", 10, cloudCB);
+    cloudHandler runing;
+    
 
     ros::spin();
 
